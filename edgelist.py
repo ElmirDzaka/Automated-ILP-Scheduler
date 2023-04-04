cc8c46014eb0ac79ccf996b9a0dcd370a9607a50
ElmirDzaka <elmir.dzaka1@gmail.com>
2023-04-03 21:30:39 -0600

    updated edgelist.py

---
+++ edgelist.py
@@ -0,0 +1,23 @@
+"""
+Authors:
+Elmir Dzaka
+Kidus Yohannes
+
+Summary:
+This file takes in an edgelist graph and processes it using NetworkX.
+
+"""
+import sys
+import networkx as nx
+
+
+def main(argv):
+    """
+    main:
+    The main function calls all helper methods and runs the program.
+    """
+    print("init")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
